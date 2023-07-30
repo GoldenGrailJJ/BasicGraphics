0d5dfea8c577e1b174955f612b3370023ffe5660
L <33853765+GoldenGrailJJ@users.noreply.github.com>
2023-07-30 10:08:37 +0800

    上传 PBR 2

---
+++ 5 PBR/ToneMapping/ToneMapping.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [ExecuteInEditorMode]
+[ExecuteInEditMode]
+public class ToneMapping : MonoBehaviour
+{
+    Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var shader = Shader.Find("Ulit/ToneMapping");
+        material = new Material(shader);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest) 
+    {
+        Graphics.Blit(src,dest,material);
+    }
+
+}
